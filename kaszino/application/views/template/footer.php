cb41c74f17d6d788ae7a1a7db59adb1d1aa5e2d7
Dragonnhun <55364375+Dragonnhun@users.noreply.github.com>
2019-11-18 16:48:42 +0100

    Szerkesztés - dice, css

--- kaszino/application/views/template/footer.php
+++ kaszino/application/views/template/footer.php
@@ -1,7 +1,9 @@
 <footer>
-    footer
+    <p>Copyright &copy Rendszergizdák <?=date("Y")?></p>
 </footer>
 
+<script src="<?=URL?>/assets/vendor/jquery/jquery-3.4.1.min.js"></script>
+<script src="<?=URL?>/assets/vendor/js/bootstrap.bundle.min.js"></script>
 <script src="<?=URL?>/assets/js/script.js"></script>
 
 </body>
--- kaszino/application/views/template/header.php
+++ kaszino/application/views/template/header.php
@@ -5,7 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?=SITE_NAME?></title>
+    <link rel="stylesheet" href="<?=URL?>/assets/vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=URL?>/assets/vendor/fontawesome/css/all.min.css">
     <link rel="stylesheet" href="<?=URL?>/assets/css/style.css">
+    <link rel="stylesheet" href="<?=URL?>/assets/css/dice.css">
 </head>
 <body>
 
