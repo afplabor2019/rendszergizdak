49cfecb7ed9784a4a54328f26d1bf7edd1fd0b91
Zsombor99 <55382782+Zsombor99@users.noreply.github.com>
2020-02-19 16:19:02 +0100

    kaszino-masodik project törlés
    felesleges

--- Kaszinó - második projekt/application/models/settings_model.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class settings_model extends Model{
-    
-    function update_user_data($id){
-        $this->executeDML("UPDATE `users` SET `name`=".$name.",`password`=".$password."WHERE `id`=".$id);
-    }
-}
